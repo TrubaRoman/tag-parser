1e019ee6198cd1669867ecc2c14933357057fb0e
Roman_T <trubaroman571@gmail.com>
2018-06-01 17:07:22 +0300

    teg-parser error vagrant reinstall

--- index.php
+++ index.php
@@ -1,3 +1,22 @@
 <?php
+if(isset($_POST['submit'])){
+  //  print_r($_POST);    die();
+    $url = $_POST['url'];
+    $tag = $_POST['tag'];
+    
+    require_once 'Parser.php';
+    $parser = new Parser();
+    $res = $parser->process($url, $tag);
+   // var_dump($res);
+}
+?>
 
-phpinfo();
+<h1>Парсер тегів по html сторінках </h1>
+<form name="parser_form" method="POST">
+    <label>Введіть адресу сторінки</label><br></br>
+    <input type="url" name="url" value="<?php echo (!empty($url))?$url:'Введіть адрес сайту тіпа http://';?>"><br></br>
+    <label>Ведіть назву тегу</label><br></br>
+    <input type="text" name="tag" value= "<?php echo (!empty($tag))?$tag:'Введіть тег тіпа h1';?>"><br></br>
+    <input type="submit" name="submit"value="відправити">
+</form>
+<?php print_r($res);?>
